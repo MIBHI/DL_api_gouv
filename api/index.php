8d41cb0bd16a3db7fc0bca937c0f294d1e344711
mibiki <mik257@hotmail.Fr>
2016-11-30 11:10:47 +0100

    base index

--- api/index.php
+++ api/index.php
@@ -1,1 +1,23 @@
 <?php
+
+$cp = 83300;
+
+if (file_exists('data/PrixCarburants_quotidien_20161123.xml')) {
+
+    $xml = simplexml_load_file('data/PrixCarburants_quotidien_20161123.xml');
+    echo '<pre>';
+    //print_r($xml);
+    echo '</pre>';
+
+
+    foreach( $xml as $pdv){
+        if($pdv->attributes()->cp == $cp){
+            echo $pdv->adresse . '<br>';
+            echo $pdv->attributes()->cp . ' ' . $pdv->ville ."<br>";
+        }
+    }
+
+} else {
+    exit('Echec lors de l\'ouverture du fichier xml.');
+}
+
